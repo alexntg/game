3fe361f25ad0df87b878b280ca220a9e676fe5b2
alex <alexterceros17@gmail.com>
2024-10-01 11:18:45 +0200

    a

--- models/characterModel.js
+++ models/characterModel.js
@@ -3,7 +3,6 @@ const path = require('path');
 
 const dataFilePath = path.join(__dirname, '../data/characters.json');
 
-// Function to get all characters from the JSON file
 exports.getAllCharacters = (callback) => {
     fs.readFile(dataFilePath, 'utf8', (err, data) => {
         if (err) {
@@ -14,10 +13,10 @@ exports.getAllCharacters = (callback) => {
         try {
             characters = JSON.parse(data);
         } catch (parseErr) {
-            console.error("Error parsing character data:", parseErr); // Log parsing error
+            console.error("Error parsing character data:", parseErr);
             return callback(parseErr);
         }
-        console.log("Retrieved characters:", characters); // Log retrieved characters
+        console.log("Retrieved characters:", characters);
         callback(null, characters);
     });
 };
@@ -29,7 +28,6 @@ exports.findCharacterById = (id, callback) => {
             return callback(err);
         }
         
-        // Ensure both `id` and `character.id` are compared as strings
         const character = characters.find(c => c.id.toString() === id.toString());
         if (!character) {
             console.log("No character found with ID:", id);
@@ -39,10 +37,9 @@ exports.findCharacterById = (id, callback) => {
     });
 };
 
-// Function to save characters to the JSON file
 exports.saveCharacters = (characters, callback) => {
-    const data = JSON.stringify(characters, null, 2); // Convert characters to JSON
+    const data = JSON.stringify(characters, null, 2);
     fs.writeFile(dataFilePath, data, 'utf8', (err) => {
-        callback(err); // Pass any error to the callback
+        callback(err); 
     });
 };
--- models/gameModel.js
+++ models/gameModel.js
@@ -1,21 +1,18 @@
 const fs = require('fs');
 const path = require('path');
 
-// Ruta al archivo JSON que contiene el estado del juego
 const gameStatePath = path.join(__dirname, '../data/gameState.json');
 
-// Función para leer el estado del juego desde el archivo JSON
 exports.getGameState = () => {
     try {
         const data = fs.readFileSync(gameStatePath, 'utf8');
         return JSON.parse(data);
     } catch (err) {
         console.error('Error reading game state:', err);
-        return { characterId: null }; // Devuelve un estado predeterminado si hay un error
+        return { characterId: null }; 
     }
 };
 
-// Función para guardar el estado del juego en el archivo JSON
 exports.saveGameState = (gameState) => {
     try {
         fs.writeFileSync(gameStatePath, JSON.stringify(gameState, null, 2), 'utf8');
